7bbbacb68370090f7ff6fa318df0d8472b06d6d9
TaiwoSokunbi <taiwo.sokunbi@truepill.com>
2024-03-20 14:01:43 +0100

    implement qa with rag techniques

--- open_api/utils.py
+++ open_api/utils.py
@@ -15,3 +15,10 @@ def ask_gemini(text, img, model='gemini-pro-vision'):
     response = llm.invoke([message])
 
     return response
+
+def calculate_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    print(f'Total Tokens: {total_tokens}')
+    print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
